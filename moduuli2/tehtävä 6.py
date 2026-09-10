65b001ffed4a7f9a67af1f05161bca288cdf8bae
Aleksi Reijonen <aleksireijonen@Mac.home>
2026-09-10 17:55:31 +0300

    päivitys kotitehtäviin

--- moduuli2/tehtävä 6.py
+++ moduuli2/tehtävä 6.py
@@ -1,3 +1,11 @@
 import random
-random_number = random.randint(1, 1000)
-print(random_number)
+random_number1= random.randint(0, 9)
+random_number2= random.randint(0, 9)
+random_number3= random.randint(0, 9)
+print((random_number1), (random_number2), (random_number3))
+
+random_num1 = random.randint(1, 6)
+random_num2 = random.randint(1, 6)
+random_num3 = random.randint(1, 9)
+random_num4 = random.randint(1, 6)
+print((random_num1), (random_num2), (random_num3), (random_num4))
